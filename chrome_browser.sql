61ed49179efb3ab9525ade48ce157ff63946d739
HarshithaAV82 <avharsitha607@gmail.com>
2026-05-07 20:52:57 +0530

    updatedelete

--- chrome_browser.sql
+++ chrome_browser.sql
@@ -4,16 +4,74 @@ use chromebrowser;
 
 create table browser_details(
 user_id char(5),
+user_name varchar(20) not null,
+website_name varchar(50),
+website_link varchar(100),
+visited_time varchar(20),
+email varchar(25)  unique,
+mobile_number bigint unique
+);
+
+alter table browser_details add password varchar(20);
+insert into browser_details(user_id, user_name, website_name, website_link, visited_time,email, mobile_number)
+values
+('u101','harshitha','youtube','www.youtube.com','10:00 a', 'avharshitha@123', 3333333333),
+('u102','hema','google','www.google.com','11:30 am' ,'avharshitha@456', 4444444444),
+('u103','harshi','instagram','www.instagram.com','1:00 pm','avharshitha@99', 5555555555);
+
+select * from browser_details;
+
+drop database chromebrowser;
+
+create database chromebrowser;
+use chromebrowser;
+
+create table browser_details(
+user_id char(5),
 user_name varchar(20),
 website_name varchar(50),
 website_link varchar(100),
 visited_time varchar(20)
 );
 
-insert into browser_details(user_id, user_name, website_name, website_link, visited_time)
-values
+insert into browser_details values('123', 'harshi', 'www.google.com', 'www.wwee.rlink', '2:30');
+
+insert into browser_details values
 ('u101','harshitha','youtube','www.youtube.com','10:00 am'),
 ('u102','hema','google','www.google.com','11:30 am'),
-('u103','harshi','instagram','www.instagram.com','1:00 pm');
+('u103','harshi','instagram','www.instagram.com','1:00 pm'),
+('104', 'hemi', 'www.link.in', 'www.likn.com', '2:00 pm');
+ 
+ 
+ insert into browser_details(user_id, user_name, website_name, website_link, visited_time)
+values('u105', 'acchu', 'X', 'www.x.in', '3:00 pm');
 
-select * from browser_details;
+insert into browser_details(user_id, user_name, website_name, website_link, visited_time)
+values('u106', 'arjun', 'y', 'www.vi.in', null),
+('107', 'harsh', 'z', 'www.zi.in', '5:00 pm'),
+('108', 'manu', 'naukri', 'naukri.com', '6:00 pm');
+
+desc browser_details;
+
+update browser_details
+ set password = 'nic@123'
+ where user_id = 'u101';
+ 
+ update browser_details
+ set password = 'nic@456'
+ where user_id = 'u102';
+ 
+ update browser_details
+ set password = 'nic@789'
+ where user_id = 'u103';
+ 
+ update browser_details
+set password = 'nic@5585'; 
+
+select * from  browser_details;
+
+delete from browser_details where user_id = '123';
+
+drop table browser_details;
+
+
---
+++ instagram.sql
@@ -0,0 +1,110 @@
+create database instagram;
+use instagram;
+
+create table users(
+user_id int not null unique,
+user_name varchar(25) unique not null,
+full_name char(25) not null,
+email varchar(50) unique not null,
+password binary(20) not null,
+bio text,
+age int,
+followers bigint,
+is_verified boolean,
+joined_year year);
+
+drop table users;
+
+insert into users values
+(1, 'hema','hema R', 'hema@gmail.com', 'hema123'
+, 'Food Blooger', 31, 30000, false,2025);
+insert into users values
+(2, 'heena', 'heena kousar','heena@gmail.com', 'henna123'
+, 'Food Blooger', 21, 40000, true,2003);
+insert into users values
+(3, 'harsha','harsha m', 'harsha@gmail.com', 'hars123'
+, 'Food Blooger', 22, 2000, false,2015);
+insert into users values
+(4, 'hamsika', 'hamsika a','hamsika@gmail.com', 'hamsika123'
+, 'Food Blooger', 61, 3000, true,2025);
+
+insert into users values
+(101, 'ariya01','Ariya R', 'ariya01@gmail.com',
+'ariya123', 'Food Blogger',31, 30000, false, 2025),
+(102, 'arbiya01','Arbiya R', 'arbiya01@gmail.com',
+'arbiya123', 'Food Blogger',31, 30000, false, 2025),
+(103, 'aya01','Aya R', 'aya01@gmail.com',
+'aya123', 'Food Blogger',31, 30000, false, 2025),
+(104, 'riya01','Riya R', 'riya01@gmail.com',
+'riya123', 'Food Blogger',31, 30000, false, 2025);
+
+
+insert into users(user_id, user_name, full_name, email, password)
+values
+(105, 'ariya','Ariya R', 'ariya@gmail.com',
+'ariya123');
+insert into users(user_id, user_name, full_name, email, 
+password)
+values
+(106, 'arbiya','Arbiya R', 'arbiya@gmail.com',
+'arbiya123');
+insert into users(user_id, user_name, full_name, email, 
+password)
+values
+(108, 'ayaaa','Ayaa R', 'ayaaa@gmail.com',
+'ayaa123');
+insert into users(user_id, user_name, full_name, email, 
+password)
+values
+(109, 'riyaaa','Riyaa R', 'riyaa@gmail.com',
+'riyaa123');              
+
+insert into  users
+(user_id, user_name, full_name, email, password)
+values
+(110, 'megha','Megha R',
+'megha@gmail.com','megha123'),
+(111, 'sneha','Sneha M',
+'sneha@gmail.com','sneha123'),
+(112, 'kiran','Kiran H',
+'kiran@gmail.com','kiran123'),
+(113, 'ravi','Ravi K',
+'ravi@gmail.com','ravi123');
+
+update users
+set bio = 'Fashion Blogger'
+where user_id = 1;
+
+update  users
+set followers = 55000
+where user_id = 2;
+
+update users
+set is_verified = false
+where user_id = 3;
+
+update users
+set full_name = 'Manu Kumar'
+where user_id = 4;
+
+update users
+set bio = 'Travel Blogger'
+where user_id = 101;
+
+DELETE FROM users
+WHERE user_id = 101;
+
+DELETE FROM users
+WHERE user_id = 102;
+
+DELETE FROM users
+WHERE user_id = 103;
+
+DELETE FROM users
+WHERE user_id = 104;
+
+DELETE FROM users
+WHERE user_id = 110;
+
+SELECT * FROM users;
+
